cf60a4797bbf2a7ceda28d552890bbb3bf41ad62
Francisco Yemail <gartija@Moby.local>
2023-02-24 11:19:38 -0500

    Adding Balanced brackets exercise solution

---
+++ problems/BalancedBrackets/Solution.java
@@ -0,0 +1,66 @@
+package problems.BalancedBrackets;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'isBalanced' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String isBalanced(String s) {
+    // Write your code here
+        Map<Character,Character> mapper = Map.of('{', '}', '(', ')', '[', ']');
+        Stack<Character> stack = new Stack<>();
+        for(Character charI : s.toCharArray()) {
+            if(mapper.containsKey(charI)) {
+                stack.push(mapper.get(charI));
+            }
+            else {
+                if(stack.empty() || !stack.pop().equals(charI)) {
+                    return "NO";
+                }
+            }
+        }
+        return stack.isEmpty()?"YES":"NO";
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                String s = bufferedReader.readLine();
+
+                String result = Result.isBalanced(s);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
